9e845330e49f09c941dbd2b80339ee6c8a50e476
WhiteTrefoil <whitetrefoil@gmail.com>
2018-02-05 13:26:19 +0800

    add bin file

--- index.js
+++ index.js
@@ -8,19 +8,29 @@ const DEFAULT_PORT = 8080
 
 const port = parseInt(process.argv[2], 10) || DEFAULT_PORT
 
-const app = new Koa()
+function run() {
+  const app = new Koa()
 
-app.use(koaBody())
+  app.use(koaBody())
 
-app.use(async(ctx, next) => {
-  await next()
+  app.use(async(ctx, next) => {
+    await next()
 
-  ctx.request.ctx.body = {
-    header: ctx.headers,
-    body  : ctx.request.body,
-  }
-})
+    ctx.request.ctx.body = {
+      header: ctx.headers,
+      body  : ctx.request.body,
+    }
+  })
 
-app.listen(port, '0.0.0.0', () => {
-  logger.info(`Server started at port ${port}`)
-})
+  app.listen(port, '0.0.0.0', () => {
+    logger.info(`Server started at port ${port}`)
+  })
+}
+
+module.exports = {
+  run: run,
+}
+
+if (require.main === module) {
+  run()
+}
